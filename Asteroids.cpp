c3741f88ae503d8ced510ba7150f7c45a49dcc81
Lion Kortlepel <development@kortlepel.com>
2020-11-06 13:29:41 +0100

    fix formatting

--- Asteroids.cpp
+++ Asteroids.cpp
@@ -2,14 +2,14 @@
 
 Rectangle rect;
 
-void init(Game &game) {
+void init(Game& game) {
     rect = Rectangle(10, 10, 200, 200, Color::Green);
 }
 
-void update(Game &game) {
+void update(Game& game) {
     rect.x += 1.0;
     game.draw_rectangle(rect);
 }
 
-void deinit(Game &game) {
+void deinit(Game& game) {
 }
