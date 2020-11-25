6b3ba13163c082cd31520660808c1d6750f04d9e
Lion Kortlepel <development@kortlepel.com>
2020-11-25 11:19:41 +0100

    fix comments

--- Asteroids.cpp
+++ Asteroids.cpp
@@ -4,10 +4,7 @@
 
 #include <iostream>
 
-// translation unit = cpp file
-
-// This is truly terrible and I am sorry for my mistakes, code Jesus please forgib for my sinz.
-// FIXME: Better alternative is to declare have player inside game object / game itself
+// heap allocate for the heck of it
 Player* player { nullptr };
 
 void init(Game&) {
